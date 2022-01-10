0b573dd8d0df33edf683fb6e276bbd724bb7916b
Niklas Högefjord <niklas@krokedil.se>
2022-01-10 11:53:45 +0100

    Display shutdown notice on Dashboard page as well

--- includes/class-dibs-admin-notices.php
+++ includes/class-dibs-admin-notices.php
@@ -4,7 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Returns error messages depending on 
+ * Returns error messages depending on
  *
  * @class    WC_Gateway_Dibs_Admin_Notices
  * @version  2.4.
@@ -13,30 +13,37 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @author   Krokedil
  */
 class WC_Gateway_Dibs_Admin_Notices {
-	
+
 	/**
 	 * WC_Gateway_Dibs_Admin_Notices constructor.
 	 */
 	public function __construct() {
 		add_action( 'admin_init', array( $this, 'check_settings' ) );
 	}
-	
+
 	public function check_settings() {
-		add_action( 'admin_notices', array( $this, 'check_old_mp_plugin' ) );
+		add_action( 'admin_notices', array( $this, 'display_shutdown_notice' ) );
 	}
-	
+
 	/**
-	 * Check if DIBS D2 MasterPass Gateway for WooCommerce is installed
+	 * Display plugin shotdown message.
 	 */
-	public function check_old_mp_plugin() {
-		
-		if( is_plugin_active('woocommerce-gateway-dibs-d2-masterpass/woocommerce-gateway-dibs-d2-masterpass.php') ) {
-			echo '<div class="notice notice-error">';
-			echo '<p>' . __( 'MasterPass is now available in the <em>DIBS for WooCommerce</em> extension. Please deactivate <em>	
-DIBS D2 MasterPass Gateway for WooCommerce</em> to avoid issues.', 'dibs-for-woocommerce' ) . '</p>';
-			echo '</div>';
+	public function display_shutdown_notice() {
+		if ( ! function_exists( 'get_current_screen' ) ) {
+			return;
 		}
+		$screen = get_current_screen();
+		if ( 'dashboard' !== $screen->id ) {
+			return;
+		}
+		ob_start();
+		?>
+		<p style="background: #ffcaca; padding: 1em; font-size: 1.1em; border: 0px solid #8c8f94; border-left:10px solid #cc0000;">
+			<strong>Please note that the <i>Nets D2 for WooCommerce</i> plugin will be retired early 2022.</strong> <br/>To continue to use Nets (previously DIBS) as your payment provider you need to upgrade to <a href="https://krokedil.com/product/nets-easy/" target="_blank">Nets Easy for WooCommerce</a>.<br/>If you don't you won't be able to accept payments when Nets D2 is closed down. <a href="https://www.nets.eu/payments/online" target="_blank">Get in touch with Nets</a> to upgrade your account. <br/>If you need help transitioning from the Nets D2 plugin to Nets Easy you can <a href="https://krokedil.com/contact/" target="_blank">contact Krokedil</a> - the developer team behind the plugins.
+		</p>
+		<?php
+		echo ob_get_clean();
 	}
-	
+
 }
-$wc_gateway_dibs_admin_notices = new WC_Gateway_Dibs_Admin_Notices;
+$wc_gateway_dibs_admin_notices = new WC_Gateway_Dibs_Admin_Notices();
